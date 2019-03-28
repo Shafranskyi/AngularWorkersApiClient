ac7fb89aa29407eea4898641a27a244d770179e6
Shafranskyi <shafranskyi_ak16@nuwm.edu.ua>
2019-03-28 17:17:03 +0200

    Ready commit

---
+++ src/app/worker.ts
@@ -0,0 +1,8 @@
+export class Worker {
+    public Id: number;
+    public FirstName: string;
+    public LastName: string;
+    public Address: string;
+    public Salary: number;
+    public Gender: string;
+}
